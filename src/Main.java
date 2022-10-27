f4c50879860655bb30edfa3caee8ec3beddd8783
Denis <108453494+DenSergeevich@users.noreply.github.com>
2022-10-27 23:21:25 +0500

    Update Main.java
    Changed the name of the variables

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Акция!За каждые 20 рублей, потраченные на билет, начисляется 1 миля.");
-        int x = 3700; // Цена билета
-        int y = 20; // Количество рублей для одной бонусной мили
+        int ticketPrice = 3700; // Цена билета
+        int forMiles  = 20; // Количество рублей для одной бонусной мили
 
-        int bonus = x / y;
+        int bonus = ticketPrice / forMiles;
         System.out.println("Количество ваших бонусных миль = " + bonus);
 
         System.out.println("Благодарим за выбор нашей авиакомании!");
